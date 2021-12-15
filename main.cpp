b977b36bb3d8b6e416af09a136833c0574c643b7
wh201906 <wh201906@yandex.com>
2021-12-15 21:16:03 +0800

    non-ascii path support
    I didn't find other solutions on the Internet.
    I found the plugins are loaded when creating MainWindow,
    so this solution should be fine?

--- main.cpp
+++ main.cpp
@@ -4,6 +4,19 @@
 
 int main(int argc, char *argv[])
 {
+#ifndef Q_OS_ANDROID
+    // A trick to handle non-ascii path
+    // The application cannot find the plugins when the path contains non ascii characters.
+    // However, the plugins will be load after creating MainWindow(or QApplication?).
+    // QDir will handle the path correctly.
+    QDir* pluginDir = new QDir;
+    if(pluginDir->cd("plugins")) // has plugins folder
+    {
+        qputenv("QT_PLUGIN_PATH", pluginDir->absolutePath().toLocal8Bit());
+    }
+    delete pluginDir;
+#endif
+
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QApplication a(argc, argv);
     QLocale loc = QLocale::system();
